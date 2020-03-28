3dc37643041c5c23ad390290a899d516ab56d4e7
andresm07 <33080733+andresm07@users.noreply.github.com>
2020-03-28 08:48:31 -0600

    Redo Quiz3

---
+++ Quiz3/Quiz3/Multiplication.swift
@@ -0,0 +1,20 @@
+//
+//  Multiplication.swift
+//  Quiz3
+//
+//  Created by admin on 3/28/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+class Multiplication {
+    var identifier = NSUUID().uuidString
+    var mainMultiplication: String
+    var multiplicationValue: Int
+    
+    init(mainMultiplication: String, multiplicationValue: Int) {
+        self.mainMultiplication = mainMultiplication
+        self.multiplicationValue = multiplicationValue
+    }
+}
---
+++ Quiz3/Quiz3/MultiplicationTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MultiplicationTableViewCell.swift
+//  Quiz3
+//
+//  Created by admin on 3/28/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class MultiplicationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var multiplicationLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    public func setupCell(myMultiplication: Multiplication) {
+        self.multiplicationLabel.text = "8 x \(myMultiplication.multiplicationValue) = \(myMultiplication.mainMultiplication)"
+    }
+    
+}
